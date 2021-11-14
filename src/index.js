013474e98dd6c07367812a05e197cc7cb541a9ce
hj357 <jkdsa9@gmail.com>
2021-11-14 19:58:33 +0900

    feat : database connection 및 router 작성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const postRouter = require("./router/post");
+const reviewRouter = require("./router/review");
+const userRouter = require("./router/user");
+
 const app = express();
 const port = 4000;
+
+app.use("/user", userRouter);
+app.use("/review", reviewRouter);
+app.use("/post", postRouter);
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!");
